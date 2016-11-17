8bebcdd4698af7e3df976b04503e37f4bb4e47a6
kesumu <kesumuZ@gmail.com>
2016-11-17 17:33:45 +0900

    Set output image name using current datetime

--- ExpressionMakerTool/MainWindow.xaml.cs
+++ ExpressionMakerTool/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace ExpressionPackageMaker
 {
@@ -42,42 +43,45 @@ namespace ExpressionPackageMaker
             this.Visibility = Visibility.Hidden;
             await Task.Delay(500);
             System.Drawing.Image screenshot = snippingtool.SnippingTool.Snip();
-            Bitmap thumb = null;
-
-            int height;
-            int width;
-            int.TryParse(height_TextBox.Text, out height);
-            int.TryParse(width_TextBox.Text, out width);
-            height = height == 0 ? 150 : height;
-            width = width == 0 ? 150 : width;
-
-            int screenshotHeight = 0;
-            int screenshotWidth = 0;
-            if (screenshot.Height*1.0 / screenshot.Width > height*1.0 / width) {
-
-                screenshotHeight = (int)(screenshot.Height * 1.0 / screenshot.Width * width);
-                screenshotWidth = width;
-            }
-            else {
-                screenshotHeight = height;
-                screenshotWidth = (int)(screenshot.Width * 1.0 / screenshot.Height * height);
-            }
-
-            thumb = new Bitmap(screenshot, screenshotWidth, screenshotHeight);
-
-            /*
-            Graphics oGraphic = Graphics.FromImage(thumb);
-
-            oGraphic.CompositingQuality = CompositingQuality.HighSpeed;
-            oGraphic.SmoothingMode = SmoothingMode.HighSpeed;
-            oGraphic.InterpolationMode = InterpolationMode.Low;
-
-            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, 200, 200);
-            oGraphic.DrawImage(screenshot, rect);
-            */
-
+            
             if (screenshot != null && snippingFolder != null && !snippingFolder.Equals("")) {
-                thumb.Save(snippingFolder + "//screenshot.png");
+                Bitmap thumb = null;
+
+                int height;
+                int width;
+                int.TryParse(height_TextBox.Text, out height);
+                int.TryParse(width_TextBox.Text, out width);
+                height = height == 0 ? 150 : height;
+                width = width == 0 ? 150 : width;
+
+                int screenshotHeight = 0;
+                int screenshotWidth = 0;
+                if (screenshot.Height * 1.0 / screenshot.Width > height * 1.0 / width)
+                {
+
+                    screenshotHeight = (int)(screenshot.Height * 1.0 / screenshot.Width * width);
+                    screenshotWidth = width;
+                }
+                else
+                {
+                    screenshotHeight = height;
+                    screenshotWidth = (int)(screenshot.Width * 1.0 / screenshot.Height * height);
+                }
+
+                thumb = new Bitmap(screenshot, screenshotWidth, screenshotHeight);
+
+                /*
+                Graphics oGraphic = Graphics.FromImage(thumb);
+
+                oGraphic.CompositingQuality = CompositingQuality.HighSpeed;
+                oGraphic.SmoothingMode = SmoothingMode.HighSpeed;
+                oGraphic.InterpolationMode = InterpolationMode.Low;
+
+                System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, 200, 200);
+                oGraphic.DrawImage(screenshot, rect);
+                */
+
+                thumb.Save(snippingFolder + "//EMT"+ DateTime.Now.ToString("yyyyMMddHHmmss") + ".png", ImageFormat.Png);
             }
             this.Visibility = Visibility.Visible;
         }
